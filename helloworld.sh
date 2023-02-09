a5b93151a60c9ec2e2f202065c71e163f36fe183
hyonluv <hyon.luv.kil@gmail.com>
2023-02-09 02:25:16 +0000

    my first push

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+# i am working perfectly
+
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author:
+# Date of latest revision:
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+# End
+
